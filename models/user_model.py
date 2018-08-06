3de74e772d832b4ba4297110b721cdcee325e073
srtipo <42049455+srtipo@users.noreply.github.com>
2018-08-06 23:33:42 +0200

    Update user_model.py

--- models/user_model.py
+++ models/user_model.py
@@ -25,3 +25,42 @@ class UserRelationship(BaseModel):
     user_secundary  = ForeignKeyField(User, backref='user_secundary')
     UserRelationshipType = IntegerField()
     created_at = DateTimeField(default=datetime.datetime.utcnow())
+
+# To find user by id
+def get_user_by_id(id):
+    try:
+        user = User.get(id==id)
+        return user
+    except User.DoesNotExist:
+        logger.debug('user %s does not exist !')
+        return None
+def get_user_by_name(user_name):
+    try:
+        user = User.get(user_name=user_name)
+        return user
+    except User.DoesNotExist:
+        logger.debug('user %s does not exist !')
+        return None
+def user_exists(id):
+    return User.select().where(User.id == id).count() > 0
+		
+	
+def get_user_by_wallet_address(address):
+    try:
+        user = Account.User.select().where(Account.User.wallet_address == address)
+        return user
+    except User.DoesNotExist:
+        logger.debug('wallet %s does not exist !', address)
+        return None
+def get_by_id( id):
+    try:
+        user = User.select().where(User.id == id).get()
+     except User.DoesNotExist:
+        return None
+
+def get_account(self):
+    try:
+        return self.account.get()
+    except Account.DoesNotExist:
+        return None
+
